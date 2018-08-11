652f00083f354950e32e1c4f535b23d339890ad4
Eduardo Corrêa <tavaresdu@gmail.com>
2018-08-10 21:48:57 -0300

    Station item creation and start of log item creation

--- bikerio/pipelines.py
+++ bikerio/pipelines.py
@@ -8,5 +8,4 @@
 
 class BikerioPipeline(object):
     def process_item(self, item, spider):
-        print(item)
         return item
--- bikerio/spiders/bikerio_map.py
+++ bikerio/spiders/bikerio_map.py
@@ -2,6 +2,8 @@
 import scrapy
 from bikerio import items
 from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+import re
 
 
 class BikerioMapSpider(scrapy.Spider):
@@ -12,13 +14,18 @@ class BikerioMapSpider(scrapy.Spider):
     def parse(self, response):
         for element in response.selector.xpath('//ul[@id="infoWind"]/li'):
             element = Selector(text=element.extract())
-            for station in element.xpath('//div[contains(@class, "infoAdd")]/text()'):
-                station = station.extract().split('-')
-                item = items.Station()
-                item['id'] = station[0].strip()
-                item['name'] = station[1].strip()
-                yield item
-        # station = items.Station()
-        # station_log = items.StationLog()
-        # for e in response.xpath(xbase + 'div[contains(@class, "infotxt")]/text()').extract():
-        #     print(e)
+            value_type = None
+            for station_log in element.xpath('//div/descendant-or-self::*/text()'):
+                line = station_log.extract().strip()
+                if re.match(r'\d+ - [\w\s]+', line):
+                    item = items.Station()
+                    station = line.split('-')
+                    item['id'] = station[0].strip()
+                    item['name'] = station[1].strip()
+                    yield item
+                elif 'Vagas Livres' in line:
+                    value_type = 'V'
+                elif 'Bicicletas Disponíveis' in line:
+                    value_type = 'B'
+                elif ':' in line:
+                    value_type = None
